d8869df781a143cb77f41c7cc89a640118acd5ba
Mohd Shahmi Abdul Majid <mshahmi@moneylion.com>
2018-02-21 15:56:44 +0800

    Adding mongo save data

--- mongolesson1/cats.js
+++ mongolesson1/cats.js
@@ -7,3 +7,22 @@ var catSchema = new mongoose.Schema({
     temperament: String
 });
 
+var Cat = mongoose.model("Cat" , catSchema);
+ //adding a new cat to the DB
+
+ var george = new Cat({
+                 name: "Cici",
+                 age: 2,
+                 temperament: "Grougcy"
+
+ });
+
+ george.save(function (err , cat ){
+     if (err){
+         console.log("SOMETHING WENT WRONG!");
+     }
+     else {
+         console.log("WE JUST SAVED A CAT TO THE DB");
+         console.log(cat);
+     }
+ });
